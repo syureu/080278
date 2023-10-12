95bb73c638db4ab07f15b7607f0aa91e0f87b199
syureu <tor01237@gmail.com>
2023-10-12 14:05:35 +0900

    3.1 <script>

--- my-projects/my-svelte-project/src/App.svelte
+++ my-projects/my-svelte-project/src/App.svelte
@@ -1,5 +1,10 @@
 <script>
-	export let name;
+	// export let name
+	export let name = "홍길동"
+	// export const name = "홍길동"
+	export function greeting(name) {
+		alert("안녕하세요 ${name}님!")
+	}
 </script>
 
 <main>
