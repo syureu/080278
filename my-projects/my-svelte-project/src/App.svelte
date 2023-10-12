bc6ae587695890f72dbf1be951fcaaf336c2ec3b
syureu <tor01237@gmail.com>
2023-10-12 14:07:14 +0900

    4.1 {#if ...} 블록

--- my-projects/my-svelte-project/src/App.svelte
+++ my-projects/my-svelte-project/src/App.svelte
@@ -1,35 +1,9 @@
+<!-- 4.1. {#if ...} 블록 -->
 <script>
-	// export let name
-	export let name = "홍길동"
-	// export const name = "홍길동"
-	export function greeting(name) {
-		alert("안녕하세요 ${name}님!")
-	}
+    let name = 'universe';
+    let a = 1; 
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#if a > 0}
+ <h1>Hello {name}!</h1>
+{/if}
