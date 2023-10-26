32c145214408f1b4eaaec89a311da9216bb23723
syureu <tor01237@gmail.com>
2023-10-26 13:38:21 +0900

    4.3 {#each ...}를 사용해 배열 표시하기

--- my-projects/my-svelte-project/src/App.svelte
+++ my-projects/my-svelte-project/src/App.svelte
@@ -1,15 +1,14 @@
-<!-- 4.2. {:else if ...} 블록 -->
+<!-- 4.3. {#each ...}를 사용해 배열 표시하기 -->
+
 <script>
-    let a = 1; 
+    let colors = ["빨강", "노랑", "파랑", "보라"]; // 반복 출력할 배열
 </script>
 
-<!-- 변수 a와 바인딩된 input 박스 -->
-<input type="number" bind:value={a}> 
-
-{#if a == 0}
- <p>{a}입니다</p>
-{:else if a > 0}
-    <p>{a} 은(는) 양수입니다</p>
-{:else}
-    <p>{a} 은(는) 음수입니다</p>
-{/if}
+<main>
+    <ul>
+        <!-- 배열의 요소 개수만큼 반복 실행 -->
+        {#each colors as color} 
+            <li>{color}</li>    <!-- 배열의 요소를 li로 출력 -->
+        {/each}
+    </ul>
+</main>
