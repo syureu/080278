9aca816e1c8282923f44c9c8406d2e4115e1673f
syureu <tor01237@gmail.com>
2023-10-26 13:40:38 +0900

    4.4. {#each} 루프 안에서 인덱스 사용하기

--- my-projects/my-svelte-project/src/App.svelte
+++ my-projects/my-svelte-project/src/App.svelte
@@ -1,16 +1,10 @@
-<!-- 4.4. {#each} 루프 안에서 인덱스 사용하기 - each에서 인덱스 사용 예 -->
+<!-- 4.4. {#each} 루프 안에서 인덱스 사용하기 - each를 활용한 select box 예 -->
 
 <script>
     let colors = ["빨강", "노랑", "파랑", "보라"]; // 반복 출력할 배열
 </script>
 
 <main>
-    <select>
-        <option> 선택하세요 </option>
-        {#each colors as color, idx } <!-- 배열의 요소 개수만큼 option 출력 -->
-            <option> {idx + 1}: {color} </option> <!-- option의 콘텐츠로 idx 활용 -->
-        {/each}
-    </select>
     <ul>
         <!-- 배열의 요소 개수만큼 반복 실행 -->
         {#each colors as color, idx}  <!-- 반복 횟수 변수 idx 선언 -->
