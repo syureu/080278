1352852c99a941509fbfa358270e319358f477d1
syureu <tor01237@gmail.com>
2023-10-26 14:00:10 +0900

    4.9. {#key 표현식} 하위 컴포넌트 {/key} 1/2

--- my-projects/my-svelte-project/src/App.svelte
+++ my-projects/my-svelte-project/src/App.svelte
@@ -1,14 +1,14 @@
-<!-- 4.8. {#key 표현식} HTML {/key} -->
+<!-- 4.9. {#key 표현식} 하위 컴포넌트 {/key} 1/2 -->
 
 <script>
-    let name = 'world'; 
+    import Component from "./Component.svelte"; // 1. 하위 컴포넌트 임포트하기
+    let name = "world"; 
 </script>
 
-<input type=text bind:value={name}> <!-- 1. name을 변경하기 위한 코드 -->
+<input type=text bind:value={name}>
 
-<p>{Date()}</p> <!-- 2. 비교용 시각 -->
 
-{#key name}
-    <p>{Date()}</p> <!-- 3. 렌더링될 때 시각을 확인하기 위한 코드 -->
-    <h1>Hello {name}!</h1> 
+{#key name} 
+     <!-- <Component name={name}/> --> <!-- 2. 컴포넌트 인스턴스 생성하기 -->
 {/key}
+<Component name={name}/> <!-- 3. #key 블록 밖에서 컴포넌트 인스턴스 생성하기 -->
