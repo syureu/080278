13ba0315af27a0f2699fb26133975d5b7c47d35e
syureu <tor01237@gmail.com>
2023-12-02 19:19:18 +0900

    8.2 props 사용하기

--- my-projects/using-child/src/App.svelte
+++ my-projects/using-child/src/App.svelte
@@ -5,6 +5,13 @@
 	// 개발자 B의 코드
 	let fruit = "apple";
 	// 개발자 C의 코드
+	let color = "yellow";
+	let colorList = [
+		{ code: "red", desc: "빨강" },
+		{ code: "yellow", desc: "노랑" },
+		{ code: "green", desc: "초록" },
+		{ code: "orange", desc: "주황" },
+	];
 </script>
 
 <!-- HTML 블록 -->
@@ -14,6 +21,11 @@
 		<li>개발자 A: <CustomSelect /></li>
 		<!-- 개발자 B의 HTML 코드 -->
 		<li>개발자 B: <CustomSelect bind:choice={fruit} /> - {fruit} 선택됨</li>
+		<!-- 개발자 C의 HTML 코드 -->
+		<li>
+			개발자 C: <CustomSelect list={colorList} bind:choice={color} /> - {color}
+			선택됨
+		</li>
 	</ul>
 </main>
 
