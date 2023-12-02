a122e936d47826c0f230c1c15009544a414d6f28
syureu <tor01237@gmail.com>
2023-12-02 19:13:39 +0900

    8.2 props 사용하기

--- my-projects/using-child/src/App.svelte
+++ my-projects/using-child/src/App.svelte
@@ -1,9 +1,16 @@
+<!-- 스크립트 블록 -->
 <script>
 	import CustomSelect from "./CustomSelect.svelte";
+	// 개발자 B의 코드
+	// 개발자 C의 코드
 </script>
 
+<!-- HTML 블록 -->
 <main>
-	<CustomSelect />
+	<ul>
+		<!-- 개발자 A의 HTML 코드 -->
+		<li>개발자 A: <CustomSelect /></li>
+	</ul>
 </main>
 
 <style>
