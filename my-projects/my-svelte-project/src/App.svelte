e892b9e7cb6c824b0791fa3dfe50f62b105a6328
syureu <tor01237@gmail.com>
2023-12-07 14:12:04 +0900

    6.9 media의 속성값 바인딩하기

--- my-projects/my-svelte-project/src/App.svelte
+++ my-projects/my-svelte-project/src/App.svelte
@@ -1,73 +1,126 @@
-<!-- 6.8. {#each ...} 블록에서 속성값 바인딩하기 -->
-
+<!-- 6.9.  Media의 속성값 바인딩하기 -->
+<!-- 스크립트 블록 -->
 <script>
-    // 고객 정보가 담긴 JSON 객체를 요소로 갖는 배열 정의
-    let customers  = [
-        { disable: false, email: "hong@mail.com", mailType:"home"  , name: "홍길동", age: 25,  grade:"gold"   },
-        { disable: false, email: "kim@mail.com", mailType:"office", name: "김길동", age: 39,  grade:"silver" },
-        { disable: false, email: "lee@mail.com", mailType:"home"  , name: "이길동", age: 35,  grade:"normal" }
-    ];  
+    let paused = true; // 정지, 시작 여부 기본값은 정지
+    let currentTime = 0; // 플레이어 최초 시작 시간
+    let duration; // 영상의 총 길이
+    let volume = 0.5; // 볼륨
+    let muted = false; // 소리 꺼짐
+    let playbackRate = 1; // 재생 속도, 기본이 1
+
+    function format(seconds) {
+        if (isNaN(seconds)) return "...";
+        let minutes = Math.floor(seconds / 60); // 분
+        seconds = Math.floor(seconds % 60); // 분을 제외한 나머지 초
+        if (seconds < 10) seconds = "0" + seconds; // 초가 1자리 일경우 앞에 0을 붙임
+        return `${minutes}:${seconds}`;
+    }
 </script>
-<!-- 데이터를 표로 표현하기 -->
-<table border=1>
-    <thead>
-        <th>탈퇴여부</th><th>이메일</th><th>메일 타입</th><th>이름</th><th>나이</th><th>등급</th>
-    </thead>
-    <tbody>
-    {#each customers as cust} <!-- customers 배열 요소 개수만큼 tr 생성 -->
-        <tr>  <!-- JSON 객체의 키 값으로 td 생성 -->
-            <td>{cust.disable}</td>
-            <td>{cust.email}</td>
-            <td>{cust.mailType}</td>
-            <td>{cust.name}</td>
-            <td>{cust.age}</td>
-            <td>{cust.grade}</td>
-        </tr>
-    {/each}        
-    </tbody>
-</table>
 
-<br/>
-<!-- 인라인 에디팅 가능한 표 만들기 -->
-<table>
-    <thead>
-        <th>탈퇴여부</th><th>이메일</th><th>메일 타입</th><th>이름</th><th>나이</th><th>등급</th>
-    </thead>
-    <tbody>
-    {#each customers as cust}
-        <tr>
-            <!-- 탈퇴여부는 체크 박스로 바인딩 -->
-            <td><input type="checkbox" bind:checked={cust.disable}></td>
-            <!-- 이메일은 input 박스에 text 바인딩 -->
-            <td><input type="text" bind:value={cust.email} disabled={cust.disable}></td>
-                  <!-- 메일 타입은 라디오 그룹 바인딩 -->
-            <td>[ 집: <input type="radio" bind:group={cust.mailType} 
-                             value="home" disabled={cust.disable}> | 
-                 회사: <input type="radio" bind:group={cust.mailType}
-                             value="office" disabled={cust.disable}> ]</td>
-                   <!-- 이름은 input 박스에 text 바인딩 -->
-            <td><input type="text" bind:value={cust.name} disabled={cust.disable}></td>
-                   <!-- 나이는 input 박스에 숫자 바인딩 -->
-            <td><input type="number" bind:value={cust.age} disabled={cust.disable}></td>
-                   <!-- 등급은 단일 select 바인딩 -->
-            <td><select bind:value={cust.grade}>
-                <option value="gold">골드</option>
-                <option value="silver">실버</option>
-                <option value="normal">일반</option>
-            </select></td>
-        </tr>
-    {/each}        
-    </tbody>
-</table>
+<!-- video 엘리먼트 영역 -->
+<video
+    src="https://ppillip.github.io/github.io/unity2d.mp4"
+    bind:paused
+    bind:currentTime
+    bind:duration
+    bind:volume
+    bind:muted
+    bind:playbackRate
+>
+    <track kind="captions" />
+</video>
 
+<!-- 컨트롤러 영역 -->
+<div class="controller">
+    <!-- 프로그레스 바 -->
+    <input
+        class="progress"
+        type="range"
+        bind:value={currentTime}
+        min="0"
+        max={duration}
+        step="0.001"
+    />
+
+    <div class="time">
+        <span>{format(currentTime)}</span><!-- 진행 시간 -->
+        <span>{format(duration)}</span>
+        <!-- 전체 시간 -->
+    </div>
+    <br />
+
+    <!-- 제어 패널 -->
+    <div class="controls">
+        <!-- 시작 / 정지 버튼 -->
+        <button
+            on:click={() => {
+                paused = !paused;
+            }}
+            >{#if paused}
+                시작
+            {:else}
+                정지
+            {/if}</button
+        >
+        <!-- 재생 속도 조절 -->
+        <select bind:value={playbackRate}>
+            <option value={0.25}>0.25배속</option>
+            <option value={0.5}>0.5배속</option>
+            <option value={1}>1배속</option>
+            <option value={2}>2배속</option>
+            <option value={3}>3배속</option>
+        </select>
+
+        <!-- 건너뛰기 기능 버튼 -->
+        <button
+            on:click={() => {
+                currentTime = currentTime - 5;
+            }}
+        >
+            &lt; 5초
+        </button>
+        <button
+            on:click={() => {
+                currentTime = currentTime + 5;
+            }}
+        >
+            5초 &gt;
+        </button>
+
+        <!-- 소리 끄기 버튼 -->
+        <button
+            on:click={() => {
+                muted = !muted;
+            }}
+            >{#if muted}
+                소리켜기
+            {:else}
+                쉿!
+            {/if}</button
+        >
+
+        <!-- 볼륨 조절 -->
+        <input type="range" bind:value={volume} min="0" max="1" step="0.01" />
+        볼륨: {volume}
+    </div>
+</div>
+
+<!-- 스타일 블록 -->
 <style>
-  table {
-    width: 100%;               /* 가로 사이즈 */
-    border: 1px solid;         /* table 테두리 */
-    border-collapse: collapse; /* table 안쪽 선은 한줄로 표시*/
-  }
-  th, td {
-    border: 1px solid;         /* 선 두께 1px, solid는 일반적인 선 */
-    padding: 10px;             /* 선과 안쪽 내용의 거리를 10px */
-  }
+    video,
+    .controller,
+    .progress,
+    .controls {
+        width: 100%;
+    }
+
+    .time {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+    }
+
+    .controls > input {
+        vertical-align: middle;
+    }
 </style>
