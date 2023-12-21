b81f1342d8eec059be255dee5bed8a01abba4ecd
syureu <tor01237@gmail.com>
2023-12-21 14:18:02 +0900

    9.1 돔 이벤트 처리하기

--- my-projects/event-control/src/App.svelte
+++ my-projects/event-control/src/App.svelte
@@ -3,11 +3,18 @@
 	let count = 0;
 	// 버튼 이벤트 처리 함수
 	const search = () => {
-		console.log("조회 버튼 클릭~!"); // 이벤트 대상을 출력
+		console.log("search: 조회 버튼 클릭~!"); // 텍스트 수정
+		count = count + 1;
+	};
+
+	// 버튼 이벤트 처리 함수 한 개 더!
+	const increase = () => {
+		console.log("increase: 조회 버튼 클릭~!");
 		count = count + 1;
 	};
 </script>
 
-<button on:click={search}>
+<button on:click={search} on:click={increase}>
+	<!-- search, increase 두 개의 함수 연결-->
 	Search ({count}) <!-- 클릭 횟수를 버튼에 표시 -->
 </button>
