cd59da91458382d0e586926d10438599e1f45097
syureu <tor01237@gmail.com>
2023-12-28 14:07:22 +0900

    10.1 onMount

---
+++ my-projects/lifecycle/src/Child.svelte
@@ -0,0 +1,13 @@
+<script>
+    //document.getElementById("text01").focus();
+    import { onMount, beforeUpdate, afterUpdate, onDestroy } from "svelte";
+
+    // onMount 예제 코드
+    onMount(() => {
+        //      document.getElementById("text01").focus();
+        console.log("Child onMount 호출됨");
+    });
+    onDestroy(() => {
+        console.log("Child onDestroy 호출됨");
+    });
+</script>
