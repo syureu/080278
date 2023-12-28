e536cc5a3c58c3333d0817c4bbb3853e76f325f7
syureu <tor01237@gmail.com>
2023-12-28 14:19:43 +0900

    10.3 beforeUpdate와 afterUpdate

--- my-projects/lifecycle/src/Child.svelte
+++ my-projects/lifecycle/src/Child.svelte
@@ -1,22 +1,46 @@
 <script>
-    // 테스트용 input 박스에 포커스 주기
-    // document.getElementById("text01").focus();
-
     let txt; // input 박스 위치 저장용 전역 변수 생성
 
-    import { onMount, beforeUpdate, afterUpdate, onDestroy } from "svelte";
+    // svelte 컴포넌트 임포트, 라이프 사이클 함수 4종 꺼내기
+    // 1. tick import 하기
+    import {
+        tick,
+        onMount,
+        beforeUpdate,
+        afterUpdate,
+        onDestroy,
+    } from "svelte";
 
     // onMount 예제 코드
     onMount(() => {
-        txt = document.getElementById("text01");
-        txt.focus();
+        txt = document.getElementById("text01"); // 돔에서 엘리먼트 찾아오기
+        txt.focus(); // 포커스 주기
         console.log("Child onMount 호출됨");
     });
+
+    // onDestory 예제 코드
     onDestroy(() => {
         console.log("Child onDestroy 호출됨");
     });
+
+    // beforeUpdate 예제 코드
+    beforeUpdate(() => {
+        console.log("Child beforeUpdate 호출됨", x); // 바인딩 변수 x의 변화를 확인
+    });
+
+    // afterUpdate 예제 코드
+    afterUpdate(() => {
+        console.log("Child afterUpdate 호출됨", x); // 바인딩 변수 x의 변화를 확인
+    });
+    let x = 0; //바인딩 변수 x를 선언
 </script>
 
 <h1>Child</h1>
-<input type="text" id="text01" />
+<input type="text" id="text01" bind:value={x} />
 <!-- 테스트용 input 박스 -->
+
+<button
+    on:click={() => {
+        x = x + 1; //클릭할 때 마다 x값 1씩 증가
+    }}>add</button
+>
