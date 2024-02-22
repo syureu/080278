b09b20610174f93832d05a3015bc5e603c4c11dc
syureu <tor01237@gmail.com>
2024-02-22 14:14:08 +0900

    11.2 readable 스토어 사용하기

--- my-projects/store/src/App.svelte
+++ my-projects/store/src/App.svelte
@@ -1,7 +1,15 @@
 <script>
+	// 1. 꺼내기
 	import { count } from "./store.js";
 	import Increase from "./Increase.svelte"; // count 증가 기능 컴포넌트
 	import Decrease from "./Decrease.svelte"; // count 감소 기능 컴포넌트
+
+	// 5. time 추가
+	import Time from "./Time.svelte";
+
+	// 6. flag 변수 생성: 하위 템플릿 생성을 결정함
+	let flag1 = false;
+	let flag2 = false;
 </script>
 
 <main>
@@ -13,5 +21,20 @@
 		<!-- 감소 / 증가 버튼 인스턴스 생성 -->
 		<Decrease />
 		<Increase />
+	</section>
+
+	<section>
+		<br />
+		<input type="checkbox" bind:checked={flag1} /> 시간 표시 1
+		{#if flag1}
+			<!-- 체크 여부에 따라 Time 컴포넌트 토글 -->
+			<Time />
+		{/if}
+		<br />
+		<input type="checkbox" bind:checked={flag2} /> 시간 표시 2
+		{#if flag2}
+			<!-- 체크 여부에 따라 Time 컴포넌트 토글 -->
+			<Time />
+		{/if}
 	</section>
 </main>
--- my-projects/store/src/store.js
+++ my-projects/store/src/store.js
@@ -1,8 +1,18 @@
-// 1. 스토어 패키지를 임포트 합니다.
-import { writable } from "svelte/store";
+// readable 꺼내기
+import { writable, readable } from "svelte/store";
 
-// 2. Count 변수를 writable로 적용하고 10으로 설정합니다.
 export const count = writable(10);
 
-// 3. 로그를 찍어 봅니다.
 console.log("스토어", count);
+
+export const time = readable(new Date(), function start(set) {
+    console.log("구독이 시작되었습니다");
+    const interval = setInterval(() => {
+        set(new Date());
+    }, 1000);
+
+    return function stop() {
+        console.log("모든 구독이 종료되었습니다");
+        clearInterval(interval);
+    };
+});
