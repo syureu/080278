1510db42874d93251c1edf24f1c2fb95907810e7
syureu <tor01237@gmail.com>
2024-02-22 14:07:13 +0900

    11.1 writable 스토어로 시작하기

--- my-projects/store/src/App.svelte
+++ my-projects/store/src/App.svelte
@@ -1,33 +1,10 @@
 <script>
-	let rangeValue = 50; // 레인지 초기값
-
-	//1. 파일 임포트 store.js에서 count를 꺼낸다
 	import { count } from "./store.js";
-	// svelte 임포트, 라이프사이클 함수 onDestroy 꺼냄
-	import { onDestroy } from "svelte";
-
-	//2. 구독(count 값 변경시 rangeValue 업데이트 하기)
-	const unsubscribe = count.subscribe((v) => {
-		rangeValue = v;
-		console.log("변경 감지");
-	});
-
-	//3. 구독 종료하기
-	onDestroy(unsubscribe);
-
-	//4. 스토어 값 변경하기 (rangeValue 값 변경시 count 도 없데이트 하기)
-	$: count.set(rangeValue);
 </script>
 
 <main>
 	<section>
-		<h2>count는 {rangeValue}입니다.</h2>
-		<input
-			type="range"
-			bind:value={rangeValue}
-			step="1"
-			min="1"
-			max="100"
-		/>
+		<h2>count는 {$count}입니다.</h2>
+		<input type="range" bind:value={$count} step="1" min="1" max="100" />
 	</section>
 </main>
