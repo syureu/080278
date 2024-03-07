34e295ed8a6c2b4731b258ea8160a77b17c2c5da
syureu <tor01237@gmail.com>
2024-03-07 14:02:05 +0900

    12.1 트윈

--- my-projects/motionSpring/src/App.svelte
+++ my-projects/motionSpring/src/App.svelte
@@ -1,30 +1,40 @@
 <script>
-	export let name;
+	// tweened 사용 선언
+	import { tweened } from "svelte/motion";
+	// easing 효과 중 cubicIn 사용 선언
+	import { cubicIn } from "svelte/easing";
+	// tweened를 통하여 progress 스토어 생성, duration 옵션 사용
+	const progress = tweened(0, {
+		duration: 1000,
+	});
+
+	// 스토어의 값이 변하는 추이를 보기 위해 로그 출력
+	progress.subscribe((v) => console.log(v));
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<progress value={$progress}></progress>
+<!-- 프로그레스 바 -->
+<button
+	on:click={() => {
+		// 0으로 움직이게 하는 버튼
+		$progress = 0;
+	}}>0%</button
+>
+<button
+	on:click={() => {
+		// 0.5로 움직이게 하는 버튼
+		progress.set(0.5);
+	}}>50%</button
+>
+<button
+	on:click={() => {
+		// 1로 움직이게 하는 버튼
+		progress.set(1);
+	}}>100%</button
+>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	progress {
+		width: 100%;
 	}
 </style>
